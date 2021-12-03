2b75efebc20b5ec1927282f6275b1e3ad422734d
Ross Simpson <simpson.ross@gmail.com>
2021-12-03 20:57:43 +0200

    Split sample data into header file

--- day_1/main.cpp
+++ day_1/main.cpp
@@ -4,9 +4,6 @@
 #include "input_data.h"
 
 int main() {
-    // Sample Data
-    // const std::vector<int> depth_map = {199, 200, 208, 210, 200, 207, 240, 269, 260, 263};
-
     uint32_t last_depth = INT32_MIN;
     uint32_t total_inc = 0;
 
---
+++ day_1/sample_data.h
@@ -0,0 +1,10 @@
+//
+// Created by rsimpson on 2021/12/03.
+//
+
+#ifndef DAY_1_SAMPLE_DATA_H
+#define DAY_1_SAMPLE_DATA_H
+
+const std::vector<int> depth_map = {199, 200, 208, 210, 200, 207, 240, 269, 260, 263};
+
+#endif //DAY_1_SAMPLE_DATA_H
