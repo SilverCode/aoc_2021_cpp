efe603609caf56bc2062499e0bb79f64a9939ec4
Ross Simpson <simpson.ross@gmail.com>
2022-03-15 15:18:20 +0200

    Day 3 Part 2

--- day_3/main.cpp
+++ day_3/main.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <vector>
 #include <bitset>
+#include <algorithm>
 
 #include "input_data.h"
 //#include "sample_data.h"
 
 using namespace std;
 
-uint32_t get_common_bit(const vector<uint32_t> &input, const int &position) {
+uint32_t get_common_bit(const vector<uint32_t> &input, const int &position, bool most_common = true) {
     int zero = 0;
     int one = 0;
     const int shift_count = BIT_COUNT - position;
@@ -21,7 +22,12 @@ uint32_t get_common_bit(const vector<uint32_t> &input, const int &position) {
         }
     }
 
-    return one > zero ? 0x1 : 0x0;
+    if (most_common) {
+        return one > zero ? 0x1 : one == zero ? -1 : 0x0;
+    } else {
+        return one < zero ? 0x1 : one == zero ? -1 : 0x0;
+    }
+
 }
 
 uint32_t flip_bits(uint32_t input) {
@@ -29,6 +35,49 @@ uint32_t flip_bits(uint32_t input) {
     output.flip();
     return static_cast<uint32_t>(output.to_ulong());
 }
+
+vector<uint32_t> oxygen_generator_rating(const vector<uint32_t> &input, int bit_position) {
+    if (input.size() <= 1) {
+        return input;
+    }
+
+    uint32_t common_bit = get_common_bit(input, bit_position);
+
+    if (common_bit == -1) {
+        common_bit = 1;
+    }
+
+    vector<uint32_t> tmp;
+    copy_if(input.begin(), input.end(), back_inserter(tmp),
+            [bit_position, common_bit](int i) {
+                uint32_t shifted = (i >> BIT_COUNT - bit_position) & 0x01;
+                return shifted == common_bit;
+            });
+
+    return oxygen_generator_rating(tmp, ++bit_position);
+}
+
+vector<uint32_t> co2_scrubber_rating(const vector<uint32_t> &input, int bit_position) {
+    if (input.size() <= 1) {
+        return input;
+    }
+
+    uint32_t least_common_bit = get_common_bit(input, bit_position, false);
+
+    if (least_common_bit == -1) {
+        least_common_bit = 0;
+    }
+
+    vector<uint32_t> tmp;
+    copy_if(input.begin(), input.end(), back_inserter(tmp),
+            [bit_position, least_common_bit](int i) {
+                uint32_t shifted = (i >> BIT_COUNT - bit_position) & 0x01;
+                return shifted == least_common_bit;
+            });
+
+    return co2_scrubber_rating(tmp, ++bit_position);
+}
+
 
 int main() {
     uint32_t gamma_rate = 0;
@@ -43,6 +92,15 @@ int main() {
     cout << "Gamma Rate: " << gamma_rate << endl;
     cout << "Epsilon Rate: " << epsilon_rate << endl;
     cout << "Power Consumption: " << epsilon_rate * gamma_rate << endl;
+
+    uint32_t oxygen = oxygen_generator_rating(report_data, 1)[0];
+    cout << "Oxygen Value: " << oxygen << endl;
+
+    uint32_t co2_scrubber = co2_scrubber_rating(report_data, 1)[0];
+    cout << "Scrubber Value: " << co2_scrubber << endl;
+
+    uint32_t life_support = co2_scrubber * oxygen;
+    cout << "Life Support Rating: " <<  life_support << endl;
 
     return 0;
 }
